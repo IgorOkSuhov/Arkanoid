73f9963e1b5845dff0610547493ac7ed0a5efa16
IgorOk <igoroksuhov060195@gmail.com>
2022-05-12 18:23:57 +0200

    Create game object and traine OOP skills=)

---
+++ Arkanoid_Game/Arkanoid.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import time
+import random
+# Create game window
+
+tk = Tk()
+tk.title('Game')
+tk.resizable(0,0) # size window
+tk.wm_attributes('-topmost',1)
+canvas = Canvas(tk,width=500, height=400,highlightthickness=0)
+canvas.pack()
+tk.update()
+
+# Window is done, right now need create game object end traine my OOP skills =)
+
+
+
