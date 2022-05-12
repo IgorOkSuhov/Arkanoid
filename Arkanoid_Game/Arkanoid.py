15a67e60b0e36f336b8ef664c1c7c460d6abaafa
IgorOk <igoroksuhov060195@gmail.com>
2022-05-12 19:10:36 +0200

    Create object Platform

--- Arkanoid_Game/Arkanoid.py
+++ Arkanoid_Game/Arkanoid.py
@@ -27,8 +27,30 @@ class Ball:
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
         self.hit_bottom = False
-        # crete function from hit ball at platform
+        # create function from hit ball at platform
         def hit_paddle(self,pos):
             paddle_pos = self.canvas.coords(self.paddle.id)
-            
+            if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+                if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                    self.score.hit() # Thats is counter points if as ball is hit platform count +1
+                    return True
+            return False # if not hit - False
+        # create function from draw as ball at game windows
+        def draw(self):
+            self.canvas.move(self.id,self.x,self.y)
+            pos = self.canvas.coords(self.id)
+            if pos[1] <= 0:
+                self.y = 2
+            if pos[3] >= self.canvas_height:
+                self.hit_bottom = True
+                # massage if players lose
+                canvas.create_text(250,120,text = 'Game Over =(',font = ('Courier', 30),fill = 'red')
+            if self.hit_paddle(pos) == True:
+                self.y = -2
+            if pos[0] <= 0:
+                self.x = 2
+            if pos [2] >= self.canvas_width:
+                self.x = -2
+# create object platform
+
 
