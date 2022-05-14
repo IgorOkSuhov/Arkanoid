1de6ba7a458e5a74ee26afdd5be2ddedfd8ba599
IgorOk <igoroksuhov060195@gmail.com>
2022-05-14 13:31:26 +0200

    Create object counter Score Player

--- Arkanoid_Game/Arkanoid.py
+++ Arkanoid_Game/Arkanoid.py
@@ -52,5 +52,38 @@ class Ball:
             if pos [2] >= self.canvas_width:
                 self.x = -2
 # create object platform
+class Paddle:
+    def __init__(self,canvas,color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0,0,100,10, fill = color)
+        start_1 = [40,60,90,120,150,180,200]
+        random.shuffle(start_1)
+        self.starting_point_x = start_1[0]
+        self.canvas.move(self.id,self.starting_point_x,300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        # control setings platform
+        self.canvas.bind_all('<KeyPress-Right>',self.turn_right)
+        self.canvas.bind_all('<KeyPress-Left>',self.turn_left)
+        #if game is not starting, just wait
+        self.started = False
+        # if player press star, game will started
+        self.canvas.bind_all('<KeyPress-Return>',self.start_game)
+    def turn_right(self,event):
+        self.x = 2
+    def turn_left(self,event):
+        self.x = -2
+    def start_game(self,event):
+        self.started = True
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        # if platforn in end window, we need stop platform
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+# create object Score
+
 
 
