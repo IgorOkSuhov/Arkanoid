e7e039ead37dab8740028cb63e5a00ea0347ae52
IgorOk <igoroksuhov060195@gmail.com>
2022-05-14 13:51:20 +0200

    Create Game Settings

--- Arkanoid_Game/Arkanoid.py
+++ Arkanoid_Game/Arkanoid.py
@@ -84,6 +84,14 @@ class Paddle:
         elif pos[2] >= self.canvas_width:
             self.x = 0
 # create object Score
-
+class Score:
+    def __init__(self,canvas,color):
+        self.score = 0
+        self.canvas = canvas
+        self.id = canvas.create_text(450,10,text=self.score, font=('Courier', 15), fill = color)
+    def hit(self):
+        self.score += 1
+        self.canvas.itemconfig(self.id, text = self.score)
+        
 
 
